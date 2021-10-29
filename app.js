b49a469ab7dc4badf2b88e99ee44f5ebd020c7fe
Minchonang <84889516+Minchonang@users.noreply.github.com>
2021-10-29 22:02:15 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
+canvas.width = 1000;
+canvas.height = 1000;
 
-ctx.strokeStyle = "black";
-ctx.lineWidth = 2.5;
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const paintMode = document.getElementById("jsPaintMode");
+
+ctx.strokeStyle = "black"; // 기본 색상
+ctx.lineWidth = 2.5; // 기본 굵기
 
 let painting = false;
+let filling = false;
 
 function paintingTrue() {
 	painting = true;
@@ -19,6 +26,9 @@ function mouseMoving(event) {
 	if (!painting) {
 		ctx.beginPath();
 		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
 	}
 }
 
@@ -30,9 +40,41 @@ function mouseDragStop(event) {
 	paintingFalse();
 }
 
+function changeColorClick(event) {
+	const selectedColor = event.target.style.backgroundColor;
+	ctx.strokeStyle = selectedColor;
+}
+
+function rangeChange(event) {
+	const changedRange = event.target.value;
+	ctx.lineWidth = changedRange;
+}
+
+function changeMode(event) {
+	if (filling === true) {
+		filling = false;
+		paintMode.innerText = "Fill";
+	} else {
+		filling = true;
+		paintMode.innerText = "Paint";
+	}
+}
+
 if (canvas) {
 	canvas.addEventListener("mousemove", mouseMoving);
 	canvas.addEventListener("mousedown", mouseDrag);
 	canvas.addEventListener("mouseup", mouseDragStop);
 	canvas.addEventListener("mouseleave", mouseDragStop);
+}
+
+Array.from(colors).forEach((color) =>
+	color.addEventListener("click", changeColorClick)
+);
+
+if (range) {
+	range.addEventListener("input", rangeChange);
+}
+
+if (paintMode) {
+	paintMode.addEventListener("click", changeMode);
 }
